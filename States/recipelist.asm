cee51b472fbe88e2f1d243f1a992138e94a402c6
ianbjohn <ianbjohn@umail.iu.edu>
2018-07-23 19:28:57 -0400

    Added strings for the craftable item recipes, wrote a pseudocode outline for the recipe list state.

--- States/recipelist.asm
+++ States/recipelist.asm
@@ -1,3 +1,25 @@
+CraftItemsMap:
+	;sorted by craftable item ID
+	;maps craftable items to their regular item counterparts
+	.db ITEM_SPEAR,ITEM_TORCH,ITEM_TOURNIQUET
+	
+ItemRecipes:
+	.dw SpearRecipe,TorchRecipe,TourniquetRecipe
+	
+SpearRecipe:
+	.db S,T,I,C,K, SPA, $2E, SPA, S,T,O,N,E,$FF
+TorchRecipe:
+	.db S,T,I,C,K, SPA, $2E, SPA, F,L,I,N,T,$FF
+Tourniquet:
+	.db S,T,I,C,K, SPA, $2E, SPA, C,L,O,T,H,$FF
+
+
 RecipeListInit:
+	;Clear screen completely
+	;For however many craftable items have been discovered
+		;Draw the item string, and then under it (Or after a colon or something)
 RecipeListMain:
+	;If B is pressed
+		;Redraw status board
+		;Go back to inventory state
 	rts
